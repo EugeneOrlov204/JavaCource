316ee27ceed1a693633304464c7aac9a2a96d442
Eugene <evgeniiorlov204@gmail.com>
2021-11-03 22:50:28 +0200

    Implemented 4 Lab task.

---
+++ src/com/suitt/eorlov/assignment6/StringSolver.java
@@ -0,0 +1,14 @@
+package com.suitt.eorlov.assignment6;
+
+public class StringSolver {
+    /*У введеному рядку замінити подвійні пробіли знаками підкреслення*/
+    public static String replaceAllDoubleWhiteSpacesWithUnderscores(String str) {
+        return str.replaceAll(" {2}", "_");
+    }
+
+    /*У введеному рядку видалити усі символи, розташовані поміж першою та
+останньою крапками*/
+    public static String removeAllSymbolsBetweenFirstAndLastDots(String str) {
+        return str.replaceAll("\\..*[.*$]", "");
+    }
+}
